cf07bb229a4de6fe7b6f209e0956ca25fc6da4bb
codenut27 <krishnan.vinodh@gmail.com>
2014-12-01 18:53:35 -0500

    Updated mat

--- mat_files_for_input/txtToMat.py
+++ mat_files_for_input/txtToMat.py
@@ -3,16 +3,18 @@ import scipy.io
 
 labels = []
 sentences = []
+
 for word in open("labels.txt","r"):
 	labels.append(int(word))
 
 for line in open("sentences.txt","r"):
 	sentences.append(line)
 
-
+my_list = np.zeros((len(sentences),), dtype=np.object)
+my_list[:] = sentences
 trainSetSize = int(0.7*len(labels))
-trainingLabels = np.array(labels[:trainSetSize])
-testingLabels = np.array(labels[trainSetSize:])
+trainingLabels = np.array(labels[:trainSetSize],dtype=float).T
+testingLabels = np.array(labels[trainSetSize:],dtype=float).T
 
-scipy.io.savemat('twitter_all.mat', mdict={'trainingLabels': trainingLabels,'testingLabels':testingLabels,'sentences':sentences})
+scipy.io.savemat('twitter_all.mat', mdict={'trainingLabels': trainingLabels,'testingLabels':testingLabels,'sentences':my_list})
 
